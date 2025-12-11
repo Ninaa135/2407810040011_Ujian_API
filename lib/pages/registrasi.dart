1cab7daec2ded0a6141ecb3d4980e4bf83a0775c
Nninaa135 <nina.lp3i.krw24@gmail.com>
2025-12-11 19:35:46 +0700

    kedua

--- lib/pages/registrasi.dart
+++ lib/pages/registrasi.dart
@@ -8,8 +8,44 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController _firstNameController = TextEditingController();
+
+  Widget _buildInputField(
+    String label,
+    TextEditingController controller, {
+    bool isNumber = false,
+    bool isEmail = false,
+  }) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: isNumber
+          ? TextInputType.number
+          : (isEmail ? TextInputType.emailAddress : TextInputType.text),
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Halaman Registrasi')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildInputField('First Name', _firstNameController),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
